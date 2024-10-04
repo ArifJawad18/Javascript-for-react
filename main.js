762824981b9b3c4ad4a3338bba1f503b74dfbffc
Arifjawad18 <arif.jawad18@gmail.com>
2024-10-05 00:22:00 +0600

    objects

---
+++ main.js
@@ -0,0 +1,41 @@
+//1.let
+const fatherName = "Arnold";
+let season = "rainy";
+season = "winter";
+
+//2. template string, arrow function, spread operator //
+
+const newNumber = [number];
+
+numbers.push(99);
+number.push(99);
+number.push(99);
+
+console.log(newNumber);
+
+//3.Array methods  map, forEach, filter and find
+const products = [
+  { name: "camera", price: 9000, brand: "canon", color: "silver" },
+  { name: "laptop", price: 70000, brand: "dell", color: "black" },
+  { name: "phone", price: 66000, brand: "vivo", color: "gray" },
+];
+const brand = products.map((product) => products);
+
+const brands = products.map((product) => product.brand);
+
+
+// 1.array destructuring
+const numbers = [42,65];
+const x = numbers[0];
+const y = numbers[1];
+
+console.log(x,y);
+
+// 4. Array and object Destructuring //
+const student ={
+  name:'Sakib',
+  age:32,
+  movies:['woods','city']
+
+}
+const [firstMovie, secondMovie] = student.movies;
